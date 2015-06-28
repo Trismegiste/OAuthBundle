82795f8fc921912f3e18f6817a19fc8f4fb61215
Trismegiste <flo@trismegiste.net>
2015-06-28 22:07:00 +0200

    with injected dynamic config

--- src/Trismegiste/OAuthBundle/DependencyInjection/TrismegisteOAuthExtension.php
+++ src/Trismegiste/OAuthBundle/DependencyInjection/TrismegisteOAuthExtension.php
@@ -2,10 +2,11 @@
 
 namespace Trismegiste\OAuthBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -14,6 +15,7 @@ use Symfony\Component\DependencyInjection\Loader;
  */
 class TrismegisteOAuthExtension extends Extension
 {
+
     /**
      * {@inheritDoc}
      */
@@ -22,12 +24,16 @@ class TrismegisteOAuthExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
+
+        $def = $container->getDefinition('oauth.provider.factory');
+        $def->replaceArgument(0, new Reference($config['config']));
     }
 
     public function getAlias()
     {
         return 'oauth';
     }
+
 }
