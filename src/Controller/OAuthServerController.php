3139c94a5518f7f1cb0d72bea914cfb20915391e
Trismegiste <flo@trismegiste.net>
2015-08-11 21:54:37 +0200

    securing oauth test route in prod env

--- src/Controller/OAuthServerController.php
+++ src/Controller/OAuthServerController.php
@@ -8,6 +8,7 @@ namespace Trismegiste\OAuthBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 /**
  * OAuthServerController is test server for oauth
@@ -17,6 +18,10 @@ class OAuthServerController extends Controller
 
     public function authorizeAction(Request $request)
     {
+        if (!in_array('dummy', $this->get('oauth.provider.factory')->getAvaliableProvider())) {
+            throw new AccessDeniedHttpException("U haxxor");
+        }
+
         $url = $request->get('redirect');
 
         return $this->render('TrismegisteOAuthBundle:OAuthServer:oauth_authorize.html.twig', ['redirect' => $url]);
